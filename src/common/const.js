a9d23becb220bdc1740b1fe299601dcb3e3070fd
simyeen <simyeen@naver.com>
2022-01-01 15:30:39 +0900

    Add : KT 본사 경위도 값 상수화

--- src/common/const.js
+++ src/common/const.js
@@ -1,1 +1,5 @@
-export const GOAL = { SLEEP_CENTER: "졸음 쉼터", PARKING_LOT: "주차장" };
+export const SEARCH_PLACE = {
+  SLEEP_CENTER: "졸음 쉼터",
+  PARKING_LOT: "주차장",
+};
+export const KT_CENTER = { Y: 37.359775085276, X: 127.11468651854 };
